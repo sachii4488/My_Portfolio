69b26c9bc9c6284c424a08050cce6f4827671438
D.M.S.P. Disanayaka <151613583+sachii4488@users.noreply.github.com>
2025-02-12 00:39:57 +0530

    Initial commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.svg'
+
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+       <img src={logo} alt="" />
+       <ul className="nav-menu">
+        <li>Home</li>
+        <li>About me</li>
+        <li>Services</li>
+        <li>Contact</li>
+        
+       </ul>
+       <div className="nav-connect">Connect with me</div>
+    </div>
+  )
+}
+
+export default Navbar
